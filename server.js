bdf52bebd0d78e90f6142b414207a36e8b3a0b79
Verma <shikha.m.verma@accenture.com>
2025-07-25 10:26:50 +0530

    Recreated .gitignore and removed node_modules from Git tracking

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Load env variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Sample test route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
